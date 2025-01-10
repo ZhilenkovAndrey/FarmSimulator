af4460ae3c21c9874accf84e4b43c8bd9ee3a44a
ZhilenkovAndrey <dron19812013@gmail.com>
2025-01-10 22:36:02 +0300

    2

--- fsim.go
+++ fsim.go
@@ -10,14 +10,36 @@ type eater interface {
 	eat()
 }
 
+type slipper interface {
+	slip()
+}
+
 type animal struct {
-	name string
+	name       string
+	movingType string
+	feedType1  string
+	feedType2  string
 }
 
-func (a *animal) move(name string) {
-	Printf("%s moving", a.name)
+func (a *animal) move(name string, movingType string) {
+	Printf("%s %s", a.name, a.movingType)
 }
 
-func (a *animal) eat(name string) {
-	Printf("%s eating", a.name)
+func (a *animal) eat1(name string) {
+	Printf("%s eat %s", a.name, a.feedType1)
+}
+
+func (a *animal) eat2(name string) {
+	Printf("%s eat %s", a.feedType2)
+}
+
+func (a *animal) slip(name string) {
+	Printf("%s slipping", a.name)
+}
+
+func main() {
+	cow := animal{"Cow", "top-top", "grass", "silage"}
+	horse := animal{"Horse", "gallop", "oats", "hay"}
+	dog := animal{"Dog", "run", "bone", "meat"}
+
 }
