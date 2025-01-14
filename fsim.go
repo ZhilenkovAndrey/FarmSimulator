0a886dfc089b7b0c1a409b02f22112c426dd7231
ZhilenkovAndrey <dron19812013@gmail.com>
2025-01-14 21:11:09 +0300

    3

--- fsim.go
+++ fsim.go
@@ -1,6 +1,10 @@
 package main
 
-import . "fmt"
+import (
+	. "fmt"
+	"math/rand"
+	"time"
+)
 
 type mover interface {
 	move()
@@ -21,25 +25,73 @@ type animal struct {
 	feedType2  string
 }
 
-func (a *animal) move(name string, movingType string) {
-	Printf("%s %s", a.name, a.movingType)
+func (a animal) String() string {
+	return a.name
 }
 
-func (a *animal) eat1(name string) {
-	Printf("%s eat %s", a.name, a.feedType1)
+func (a *animal) move() {
+	Printf("%s %s\n", a.name, a.movingType)
 }
 
-func (a *animal) eat2(name string) {
-	Printf("%s eat %s", a.feedType2)
+func (a *animal) eat() {
+	Printf("%s eat %s\n", a.name, a.feedChoise())
 }
 
-func (a *animal) slip(name string) {
-	Printf("%s slipping", a.name)
+func (a *animal) slip() {
+	Printf("%s slipping\n", a.name)
 }
 
-func main() {
+func (a *animal) feedChoise() string {
+	var s string
+	b := rand.Intn(2)
+	switch b {
+	case 0:
+		s = Sprintf("%s", a.feedType1)
+	case 1:
+		s = Sprintf("%s", a.feedType2)
+	}
+	return s
+}
+
+func life(an []animal) {
+	for i := 0; i < 24; i++ {
+
+		Println()
+		Printf(" TIME: %d:00\n", i)
+
+		if 0 <= i && i < 6 {
+			for j := range an {
+				an[j].slip()
+			}
+		}
+		if 6 <= i && i < 12 {
+			for j := range an {
+				an[j].eat()
+			}
+		}
+		if 12 <= i && i < 18 {
+			for j := range an {
+				an[j].move()
+			}
+		}
+		if 18 <= i && i < 24 {
+			for j := range an {
+				an[j].eat()
+			}
+		}
+
+		time.Sleep(time.Second)
+	}
+}
+
+func animalAppearence() []animal {
 	cow := animal{"Cow", "top-top", "grass", "silage"}
 	horse := animal{"Horse", "gallop", "oats", "hay"}
 	dog := animal{"Dog", "run", "bone", "meat"}
 
+	return []animal{cow, horse, dog}
+}
+
+func main() {
+	life(animalAppearence())
 }
